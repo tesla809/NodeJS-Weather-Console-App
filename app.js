8525ee0455c06b2f13ff6de3b13243d6f466c27d
Anthony Albertorio <anthony.albertorio@gmail.com>
2016-08-10 16:46:48 -0400

    added app.js iterates over weather.js to fetch the weather for the inserted zipcodes

---
+++ app.js
@@ -0,0 +1,16 @@
+// get weather
+var weather = require("./weather.js");
+
+// get zipcode arguments inside console.
+var zipcodes = process.argv.slice(2);
+
+// allows you to pass any number of zipcodes for weather
+zipcodes.forEach(weather.get);
+
+
+/*
+you could also do.
+users.forEach(function(username){
+  profile.get(username);
+});
+*/
